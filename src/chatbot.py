ba2019cda1bfea304d1ca0f3f47f0d24516da730
longluv1605 <22022604@vnu.edu.vn>
2025-06-30 14:23:38 +0700

    mod: prompt template

--- src/chatbot.py
+++ src/chatbot.py
@@ -7,6 +7,7 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain.prompts import PromptTemplate
 
 from retriever import get_history_retriever
 
@@ -20,8 +21,29 @@ def load_llm():
         api_key=os.getenv('GOOGLE_API_KEY')
     )
     
+def create_prompt_template():
+    template = """
+    [system]
+    Bạn là một trợ lý chuyên giải đáp các thắc mắc về môn lịch sử trung học phổ thông (cụ thể là lớp 12, tại Việt Nam).
+    Các câu trả lời của bạn phải liên quan đến bộ môn lịch sử và dựa vào thông tin được cung cấp.
+    Bạn chỉ được trả lời bằng tiếng Việt.
+    Nếu câu hỏi không liên quan đến môn lịch sử, hãy trả lời rằng bạn không trả lời các câu hỏi ngoài môn học.
+    
+    [user]
+    Sau đây là câu hỏi của người dùng: {input}.
+    
+    [chat history]
+    Đây là lịch sử chat của bạn và user: {chat_history}.
+    
+    [context]
+    Đây là thông tin được cung cấp: {context}.
+    """
+    return PromptTemplate.from_template(template)
+
+
 def create_qa_chain(llm, retriever):
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    # retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    retrieval_qa_chat_prompt = create_prompt_template()
     combine_docs_chain = create_stuff_documents_chain(llm=llm, prompt=retrieval_qa_chat_prompt)
     
     return create_retrieval_chain(
